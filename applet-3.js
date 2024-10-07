df4e0df0546ae2a467b09af35368e28168b03ab3
jhonlawrenceRafer <beboyrafer@gmail.com>
2024-10-07 14:57:33 +0800

    added clearLogs and CardContainer

--- applet-3.js
+++ applet-3.js
@@ -15,4 +15,23 @@ class DataLogger{
         this.loggedData.push(time);
         this.updateCardContainer();
     }
+    clearLogs(){
+        this.loggedData = []; 
+        this.updateCardContainer();
+    }
+    updateCardContainer(){
+        this.idContainer.innerHTML = '';
+    
+        this.loggedData.forEach(data => {       
+            const card = document.createElement('div');
+            card.className = 'card mb-2';
+            card.innerHTML = `
+                <div class="card-body">
+                    <h5 class="card-title">Logged Data</h5>
+                    <p class="card-text">${data}</p>
+                </div>
+            `;
+            this.idContainer.appendChild(card);
+        });
+}
 }

0288f1a869bcdec43095f82e4f843aa178cb2b29
jhonlawrenceRafer <beboyrafer@gmail.com>
2024-10-09 10:19:01 +0800

    added cons Mymap

--- applet-4.js
+++ applet-4.js
@@ -137,3 +137,10 @@ class LeafletMap {
     }
 
 }
+const Mymap = new LeafletMap('map', [8.359735, 124.869206], 18);
+
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+        Mymap.displayLogCount();
+    });

baa63facd459e0b9c75309236c6da223a3d433da
jhonlawrenceRafer <beboyrafer@gmail.com>
2024-10-11 18:50:15 +0300

    added rederStudentList

--- applet-5.js
+++ applet-5.js
@@ -12,6 +12,23 @@ class StudentList {
         this.bindSearchEvent();
 
     }
-  
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            this.students = await response.json();
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    renderStudentList(students) {
+        const studentListContainer = document.getElementById('studentList');
+        studentListContainer.innerHTML = students.map(student => 
+            `<button type="button" class="btn btn-dark" style="margin-top:15px; 
+                                                    width:25rem">
+                ${student.student_name} | ${student.student_program}
+            </button><br>`
+        ).join('');
+    }
    
 }

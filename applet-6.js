a23d0b3ef1f7501f2ec99cea25665bdbafedc388
jhonlawrenceRafer <beboyrafer@gmail.com>
2024-10-17 19:13:39 +0300

    added fetchWeather

--- applet-6.js
+++ applet-6.js
@@ -34,7 +34,27 @@ class WeatherService extends WeatherApp {
         this.apiKey = '';
     }
 
-  
+    async fetchWeather() {
+        const city = this.cityInput.value;
+        this.apiKey = this.APIKeyInput.value.trim(); 
+
+        if (!this.apiKey) {
+            alert('Please enter your API key.');
+            return;
+        }
+
+        if (city) {
+            const data = await this.getWeatherData(city);
+            if (data) {
+                this.displayWeather(data);
+            } else {
+                alert('City not found. Please try again.');
+            }
+        } else {
+            alert('Please enter a city name.');
+        }
+    }
+
    
   
 

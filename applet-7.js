cd40d725266688c9ff1b4801a60e7fd3c5a989ca
jhonlawrenceRafer <beboyrafer@gmail.com>
2024-10-30 10:29:23 +0800

    added tasktext

--- applet-7.js
+++ applet-7.js
@@ -22,6 +22,20 @@ class todoList{
         }
     }
 
-  
+    addTask(taskText) {
+        const listItem = document.createElement('li');
+        listItem.className = 'list-group-item todo-item';
+        listItem.innerHTML = `
+            <span class="task-text">${taskText}</span>
+            <span class="timestamp" style="display: block; margin-top: 0.5rem; color: gray;">Date Added: ${new Date().toLocaleString()}</span>
+            <div style="margin-top: 0.5rem;">
+                <button class="btn btn-success btn-sm doneButton">Done</button>
+                <button class="btn btn-warning btn-sm editButton">Edit</button>
+                <button class="btn btn-danger btn-sm removeButton">Remove</button>
+            </div>
+        `;
+        this.todoList.appendChild(listItem);
+    }
+
 }
 

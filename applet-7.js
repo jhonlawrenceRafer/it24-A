9fddbcfbcb6662b168162782e707ac89adf036cd
jhonlawrenceRafer <beboyrafer@gmail.com>
2024-10-30 10:30:39 +0800

    added edittask event

--- applet-7.js
+++ applet-7.js
@@ -55,6 +55,17 @@ class todoList{
         this.todoList.removeChild(event.target.closest('.todo-item'));
     }
 
-   
+    editTask(event) {
+        const taskItem = event.target.closest('.todo-item');
+        this.todoInput.value = taskItem.querySelector('.task-text').textContent;
+        this.editingIndex = Array.from(this.todoList.children).indexOf(taskItem);
+        this.addButton.textContent = 'Update';
+    }
+
+    resetEditing() {
+        this.editingIndex = -1;
+        this.addButton.textContent = 'Add';
+    }
+
 }
 

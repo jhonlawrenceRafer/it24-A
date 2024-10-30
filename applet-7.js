fe188181232cf108c261aecf2c4e5a0434cf629b
jhonlawrenceRafer <beboyrafer@gmail.com>
2024-10-30 10:30:12 +0800

    added removetask

--- applet-7.js
+++ applet-7.js
@@ -46,5 +46,15 @@ class todoList{
         buttons.forEach(button => button.disabled = true);
     }
 
+    updateTask(taskText) {
+        this.todoList.children[this.editingIndex].querySelector('.task-text').textContent = taskText;
+        this.resetEditing();
+    }
+
+    removeTask(event) {
+        this.todoList.removeChild(event.target.closest('.todo-item'));
+    }
+
+   
 }
 
